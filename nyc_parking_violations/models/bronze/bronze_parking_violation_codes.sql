551d5836f8c716ebd0fc99d6520a53556fd6bb0b
adhilcc <aahammed@ecloudcontrol.com>
2025-06-23 17:48:49 +0530

    Update bronze_parking_violation_codes.sql

--- nyc_parking_violations/models/bronze/bronze_parking_violation_codes.sql
+++ nyc_parking_violations/models/bronze/bronze_parking_violation_codes.sql
@@ -1,7 +1,7 @@
 SELECT
     code AS violation_code,
     definition,
-    Manhattan_96th_St_and_below,
-    All_Other_Areas
+    manhattan_96th_st_and_below,
+    all_other_areas
 FROM
     {{ ref('dof_parking_violation_codes') }}
