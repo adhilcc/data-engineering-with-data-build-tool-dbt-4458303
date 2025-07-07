b56e53181413051bf362a1a51427a656717148e9
adhilcc <aahammed@ecloudcontrol.com>
2025-07-07 21:54:00 +0530

    Update NYC_postgres_dag.py

--- NYC_postgres_dag.py
+++ NYC_postgres_dag.py
@@ -130,7 +130,7 @@ with DAG(
     )
 
     # Final dummy task
-    post_dbt = EmptyOperator(task_id="post_dbt")
+    post_dbt_test_success = EmptyOperator(task_id="post_dbt")
 
     # DAG execution flow
     seeds_tg >> stg_bronze >> stg_silver >> stg_gold >> post_dbt
